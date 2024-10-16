98f98b9a8727eaa35ac7f53d7673941d4ded704c
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2024-10-16 10:17:37 +0200

    add user login form 2

--- app/Http/Controllers/Auth/AuthenticateController.php
+++ app/Http/Controllers/Auth/AuthenticateController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Auth;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -16,5 +17,20 @@ class AuthenticateController extends Controller
     public function store(Request $request)
     {
         // todo : add authentication
+
+        $cardinals = $request->validate([
+            "email" => 'required|email',
+            "password" => 'required',
+        ]);
+
+        if (Auth::attempt($cardinals, $request->boolean('remember'))) {
+            $request->session()->regenerate();
+            //TODO: redirect user to the dashboard, now it redirects to the home page by default
+            return redirect()->intended();
+        }
+
+        return back()->withErrors([
+            "email" => "The provided credentials do not match our records.",
+        ])->onlyInput('email');
     }
 }
