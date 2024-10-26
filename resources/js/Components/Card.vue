43bdb93127d6ef3b8e936402976e192e5614ffba
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2024-10-26 15:51:26 +0200

    add user filter

--- resources/js/Components/Card.vue
+++ resources/js/Components/Card.vue
@@ -1,55 +1,58 @@
 <script setup>
-import TextLink from "./TextLink.vue";
+
+import {router} from "@inertiajs/vue3";
+
+const params = route().params;
 
 defineProps({
-    listing: Object,
+  listing: Object,
 });
+
+const selectUser = (id) => {
+  router.get(route('home'), {
+    user_id: id,
+    search: params.search,
+  });
+}
+
 </script>
 
 <template>
-    <div
-        class="bg-white rounded-lg shadow-lg overflow-hidden dark:bg-slate-800 h-full flex flex-col justify-between"
-    >
-        <div>
-            <!--Image-->
-            <Link href="">
-                <img
-                    :src="
-                        listing.image
-                            ? `/storage/${listing.image}`
-                            : 'storage/images/listings/default.jpg'
-                    "
-                    alt="default image"
-                    class="w-full h-48 bg-blue-200 object-cover object-center"
-                />
-            </Link>
+  <div class="bg-white rounded-lg shadow-lg overflow-hidden dark:bg-slate-800 h-full flex flex-col justify-between">
+    <div>
+      <!--Image-->
+      <Link href="">
+        <img :src="listing.image
+                ? `/storage/${listing.image}`
+                : 'storage/images/listings/default.jpg'
+                " alt="default image" class="w-full h-48 bg-blue-200 object-cover object-center"/>
+      </Link>
 
-            <!--Title & User Info -->
-            <div class="p-4">
-                <h3 class="font-bold text-xl mb-2">
-                    {{ listing.title.substring(0, 15) }}...
-                </h3>
-                <p>
-                    Listed on
-                    {{ new Date(listing.created_at).toLocaleDateString() }}
-                    by
-                    <!--Todo:change the routeName to listing by user-->
-                    <TextLink :label="listing.user.name" :routeName="'home'" />
-                </p>
-            </div>
-        </div>
-
-        <!--Tags-->
-        <div v-if="listing.tags" class="flex items-center gap-3 px-4 py-4">
-            <div v-for="tag in listing.tags.split(',')" :key="tag">
-                <button
-                    class="bg-slate-600 text-white text-sm transition-colors duration-300 px-3 py-1 rounded-full hover:bg-slate-700 dark:hover:bg-slate-850"
-                >
-                    {{ tag }}
-                </button>
-            </div>
-        </div>
+      <!--Title & User Info -->
+      <div class="p-4">
+        <h3 class="font-bold text-xl mb-2">
+          {{ listing.title.substring(0, 15) }}...
+        </h3>
+        <p>
+          Listed on
+          {{ new Date(listing.created_at).toLocaleDateString() }}
+          by
+          <!--Todo:change the routeName to listing by user-->
+          <button class="text-link" @click="selectUser(listing.user.id)">{{ listing.user.name }}</button>
+        </p>
+      </div>
     </div>
+
+    <!--Tags-->
+    <div v-if="listing.tags" class="flex items-center gap-3 px-4 py-4">
+      <div v-for="tag in listing.tags.split(',')" :key="tag">
+        <button
+            class="bg-slate-600 text-white text-sm transition-colors duration-300 px-3 py-1 rounded-full hover:bg-slate-700 dark:hover:bg-slate-850">
+          {{ tag }}
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
