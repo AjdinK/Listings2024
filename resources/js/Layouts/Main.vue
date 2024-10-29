2666553e292ade0fbc65d70511159010e9580fd0
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2024-10-29 16:53:48 +0100

    add crud listing 1

--- resources/js/Layouts/Main.vue
+++ resources/js/Layouts/Main.vue
@@ -14,48 +14,70 @@ const show = ref(false);
     <div v-show="show" class="fixed inset-0 z-40" @click="show = false"></div>
 
     <header class="bg-slate-800 text-white">
-        <nav class="p-6 mx-auto max-w-screen-lg flex items-center justify-between">
+        <nav
+            class="p-6 mx-auto max-w-screen-lg flex items-center justify-between"
+        >
             <NavLink componentName="Home" routeName="home">Home</NavLink>
 
             <div class="flex gap-4 items-center">
                 <!--Auth-->
                 <div v-if="user" class="relative">
-                    <div :class="{ 'bg-slate-700': show }"
+                    <div
+                        :class="{ 'bg-slate-700': show }"
                         class="flex items-center gap-2 px-3 py-3 rounded-lg hover:bg-slate-700 cursor-pointer"
-                        @click="show = !show">
+                        @click="show = !show"
+                    >
                         <p>{{ user.name }}</p>
                         <i class="fa-solid fa-angle-down"></i>
                     </div>
                     <!--user drop down menu-->
 
-                    <div v-show="show"
+                    <div
+                        v-show="show"
                         class="absolute z-50 top-16 right-0 bg-slate-800 text-white rounded-lg p-4 border-slate-500 overflow-hidden w-40"
-                        @click="show = false">
-                        <Link :href="route('profile.edit')"
-                            class="block w-full rounded-lg px-3 py-4 hover:bg-slate-700 text-left">Profile
+                        @click="show = false"
+                    >
+                        <Link
+                            :href="route('listing.create')"
+                            class="block w-full rounded-lg px-3 py-4 hover:bg-slate-700 text-left"
+                            >New Listing
+                        </Link>
+                        <Link
+                            :href="route('profile.edit')"
+                            class="block w-full rounded-lg px-3 py-4 hover:bg-slate-700 text-left"
+                            >Profile
                         </Link>
 
-                        <Link :href="route('dashboard')"
-                            class="block w-full rounded-lg px-3 py-4 hover:bg-slate-700 text-left">Dashboard
+                        <Link
+                            :href="route('dashboard')"
+                            class="block w-full rounded-lg px-3 py-4 hover:bg-slate-700 text-left"
+                            >Dashboard
                         </Link>
 
-                        <Link :href="route('logout')" as="button"
-                            class="block w-full rounded-lg px-3 py-4 hover:bg-slate-700 text-left" method="post">Logout
+                        <Link
+                            :href="route('logout')"
+                            as="button"
+                            class="block w-full rounded-lg px-3 py-4 hover:bg-slate-700 text-left"
+                            method="post"
+                            >Logout
                         </Link>
                     </div>
                 </div>
 
                 <!--Guest -->
                 <div v-else class="space-x-6">
-                    <NavLink componentName="Auth/Login" routeName="login">Login
+                    <NavLink componentName="Auth/Login" routeName="login"
+                        >Login
                     </NavLink>
-                    <NavLink componentName="Auth/Register" routeName="register">Register
+                    <NavLink componentName="Auth/Register" routeName="register"
+                        >Register
                     </NavLink>
                 </div>
 
                 <button
                     class="hover:bg-slate-700 w-6 h-6 grid place-items-center rounded-full hover:outline outline-1 outline-white"
-                    @click="switchTheme()">
+                    @click="switchTheme()"
+                >
                     <i class="fa-solid fa-circle-half-stroke"></i>
                 </button>
             </div>
---
+++ resources/js/Pages/Listing/Create.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { Head, useForm } from "@inertiajs/vue3";
+import Container from "../../Components/Container.vue";
+import Title from "../../Components/Title.vue";
+import InputField from "../../Components/InputField.vue";
+import TextArea from "../../Components/TextArea.vue";
+
+const form = useForm({
+    title: "",
+    description: "",
+    email: "",
+    tags: "",
+    link: "",
+    image: "",
+});
+
+</script>
+
+<template>
+
+    <Head title=" - Create New Listing" />
+    <Container>
+        <div class="mb-6">
+            <Title>Create New Listing</Title>
+        </div>
+        <form class="grid grid-cols-2 gap-6">
+
+            <div class='space-y-6'>
+                <InputField label="Title" icon="heading" placeholder="Enter Listing Title" v-model="form.title" />
+                <InputField label="Tags (seperate with comma)" icon="tags" placeholder="one,two,three"
+                    v-model="form.tags" />
+                <TextArea label="Description" icon="paragraph" placeholder="Enter Description"
+                    v-model="form.description" />
+
+            </div>
+            <div class='space-y-6'>
+                <InputField label=" Email" icon="at" placeholder="Enter Your Email" v-model="form.email" />
+                <InputField label="External Link" icon="up-right-from-squre" placeholder="https://"
+                    v-model="form.link" />
+            </div>
+        </form>
+    </Container>
+</template>
+
+<style scoped></style>
