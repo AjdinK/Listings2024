f4c566b73f8fa656c06609db9f14d0cd200bcd9f
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2024-11-03 09:03:18 +0100

    add user dashborad 2

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,7 +9,8 @@ class DashboardController extends Controller
 {
     public function index(Request $request)
     {
-        $userListing = $request->user()->listings();
+        $userListing = $request->user()->role !== 'suspended' ?
+        $request->user()->listings()->latest()->paginate(5) : null;
         return Inertia::render('Dashboard', [
             'listings' => $userListing
         ]);
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,13 +1,22 @@
 <script setup>
+import { Head } from '@inertiajs/vue3'
+
 defineProps({
     listings: Object,
 })
 </script>
 
 <template>
-    <div>
-        {{ console.log(listings) }}
-        <h1>Dashboard</h1>
+    <Head title=" Dashboard" />
+    <div v-if="listings">
+        <div v-if="Object.keys(listings.data).length"></div>
+
+        <div v-else>You Have No Listings</div>
+    </div>
+
+    <div v-else>
+        Due to violation of the Terms of Service, you are not allowed to list
+        please contact us at <span class="text-link">email@admin.com</span>
     </div>
 </template>
 
