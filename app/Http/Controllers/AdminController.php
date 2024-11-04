ae3d4d980bd93892c9c6a68a7b47c0b544c4c49b
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2024-11-04 11:36:09 +0100

    add admin dashboard 2

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class AdminController extends Controller
 {
     public function index()
-    {
-        return Inertia::render('Admin/AdminDashboard');
+{
+        $user = User::with('listings')->paginate(10)
+        return Inertia::render('Admin/AdminDashboard',[
+        'user' => $user,
+        ]);
     }
 }
