a7a697b0a880aea3186a733f693c43af07e50a6d
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2025-02-16 01:44:34 +0100

    solo config

--- config/solo.php
+++ config/solo.php
@@ -46,18 +46,21 @@ return [
     |
     */
     'commands' => [
+        
         'About' => 'php artisan solo:about',
-        'Logs' => EnhancedTailCommand::file(storage_path('logs/laravel.log')),
         'Vite' => 'npm run dev',
-        'Make' => new MakeCommand,
-        // 'HTTP' => 'php artisan serve',
-
-        // Lazy commands do no automatically start when Solo starts.
-        'Dumps' => Command::from('php artisan solo:dumps')->lazy(),
-        'Reverb' => Command::from('php artisan reverb')->lazy(),
-        'Pint' => Command::from('./vendor/bin/pint --ansi')->lazy(),
-        'Queue' => Command::from('php artisan queue:work')->lazy(),
-        'Tests' => Command::from('php artisan test --colors=always')->lazy(),
+        'HTTP' => 'php artisan serve',
+        
+        
+//        'Make' => new MakeCommand,
+//        'Logs' => EnhancedTailCommand::file(storage_path('logs/laravel.log')),
+//
+//        // Lazy commands do no automatically start when Solo starts.
+//        'Dumps' => Command::from('php artisan solo:dumps')->lazy(),
+//        'Reverb' => Command::from('php artisan reverb')->lazy(),
+//        'Pint' => Command::from('./vendor/bin/pint --ansi')->lazy(),
+//        'Queue' => Command::from('php artisan queue:work')->lazy(),
+//        'Tests' => Command::from('php artisan test --colors=always')->lazy(),
     ],
 
     /*
